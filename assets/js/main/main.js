ef9d3df94286237b78e46e89bec3e3d383360740
bemuse01 <bemuse01>
2021-01-10 08:48:55 +0900

    commit 2021.01.10.08:48

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -1,92 +1,74 @@
-// three
-const createObjectDnaUi = (app) => {
-    COMP.object.ui.dna = new CLASS.object.ui.dna(app)
-}
-const createObjectDna = (app) => {
-    COMP.object.dna = {big: [], small: []}
+new Vue({
+    el: '#wrap',
+    data(){
+        return{
 
-    const big = new PARAM.object.dna.build()
-    const small = new PARAM.object.dna.build({size: 1.1, rand: {bone: 4.0, nucleic: 3.0}, point: 60})
-
-    for(let i = 0; i < 2; i++) COMP.object.dna.big.push(new CLASS.object.dna.build(app, big))
-    for(let i = 0; i < 3; i++) COMP.object.dna.small.push(new CLASS.object.dna.build(app, small))
-}
-const buildDna = (app) => {
-
-    createObjectDna(app)
-    createObjectDnaUi(app)
-}
-const createObject = () => {
-    const app = COMP.object.app.getApp()
-
-    buildDna(app)
-}
-const animateObject = () => {
-    COMP.object.dna.big.forEach(e => e.rotationY(0.02))
-    COMP.object.dna.small.forEach(e => e.rotationY(0.02))
-}
-const renderThree = () => {
-    COMP.object.app.render()
-    animateObject()
-}
-const initThree = () => {
-    const canvas = document.querySelector('#canvas')
-
-    const param = new PARAM.object.app()
-
-    COMP.object.app = new CLASS.object.app(canvas, param)
-
-    createObject()
-}
+        }
+    },
+    mounted(){
+        this.init()
+    },
+    methods: {
+        // init
+        init(){
+            this.initThree()
+            this.animate()
+            window.addEventListener('resize', this.onWindowResize, false)    
+        },
 
 
-// event
-const onWindowResize = () => {
-    PARAM.util.width = window.innerWidth
-    PARAM.util.height = window.innerHeight
+        // three
+        initThree(){
+            const canvas = document.querySelector('#canvas')
+            const param = new PARAM.object.app()
 
-    COMP.object.app.resize()
-}
+            COMP.object.app = new CLASS.object.app(canvas, param)
+
+            this.createObject()
+        },
+        renderThree(){
+            COMP.object.app.render()
+            this.animateObject()
+        },
+        // create object
+        createObject(){
+            const app = COMP.object.app.getApp()
+
+            this.createObjectDna(app)
+        },
+        createObjectDna(app){
+            COMP.object.dna = {big: [], small: []}
+
+            const big = new PARAM.object.dna.build()
+            const small = new PARAM.object.dna.build({size: 1.1, rand: {bone: 4.0, nucleic: 3.0}, point: 60})
+
+            for(let i = 0; i < 2; i++) COMP.object.dna.big.push(new CLASS.object.dna.build(app, big))
+            for(let i = 0; i < 3; i++) COMP.object.dna.small.push(new CLASS.object.dna.build(app, small))
+        },
+        // animate object
+        animateObject(){
+            COMP.object.dna.big.forEach(e => e.rotationY(0.02))
+            COMP.object.dna.small.forEach(e => e.rotationY(0.02))
+        },
 
 
-// render
-const render = () => {
-    renderThree()
-    TWEEN.update()
-}
-const animate = () => {
-    render()
-    requestAnimationFrame(animate)
-}
+        // event
+        onWindowResize(){
+            PARAM.util.width = window.innerWidth
+            PARAM.util.height = window.innerHeight
+        
+            COMP.object.app.resize()
+        },
 
 
-// init
-const init = () => {
-    const t = 1 / 80
-    const f = BezierEasing(0.645, 0.045, 0.355, 1.000)
-    const duration = 500
-    for(let i = 0; i < 80; i++){
-        const easing = f(i * t)
-        console.log(easing * duration)
+        // render
+        render(){
+            this.renderThree()
+            TWEEN.update()
+        },
+        animate(){
+            this.render()
+            requestAnimationFrame(this.animate)
+        }
     }
-
-    initThree()
-    animate()
-    window.addEventListener('resize', onWindowResize, false)    
-}
-init()
-
-// new Vue({
-//     el: '#wrap',
-//     data(){
-//         return{
-
-//         }
-//     },
-//     mounted(){
-
-//     },
-//     methods: {
-
-//     }
-// })
+})
